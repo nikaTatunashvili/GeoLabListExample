56a76a0f4558ea074eb7f89fe9c789db30b87f0f
nikaTatunashvili <66839606+nikaTatunashvili@users.noreply.github.com>
2020-06-12 20:31:43 +0400

    Update ViewController.swift

--- List Example/ViewController.swift
+++ List Example/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  List Example
-//
-//  Created by GL on 6/12/20.
-//  Copyright © 2020 GL. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
