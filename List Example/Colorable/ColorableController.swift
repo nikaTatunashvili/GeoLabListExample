d5c8ac422ad185e291fda87db796e47a3118c6a9
List <nika.tatunashvili@geolab.edu.ge>
2020-06-17 20:38:35 +0400

    add tableview cell actions

---
+++ List Example/Colorable/ColorableController.swift
@@ -0,0 +1,21 @@
+//
+//  ColorableController.swift
+//  List Example
+//
+//  Created by GL on 6/17/20.
+//  Copyright © 2020 GL. All rights reserved.
+//
+
+import UIKit
+
+class ColorableController: UIViewController {
+    var backgroundColor: UIColor = .white
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = backgroundColor
+        
+    }
+
+}
--- List Example/ViewController.swift
+++ List Example/ViewController.swift
@@ -4,17 +4,68 @@ class ViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
 
-    var texts = ["tett", "kjghksdj", "nkjdhkd"]
+    var texts = ["Green controller", "Red controller", "Blue Controller"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
+    }
+
+    func goToColorable(color: UIColor) {
+        let vc = ColorableController()
+        vc.backgroundColor = color
+        self.present(vc, animated: true, completion: nil)
+    }
+
+    func deleteRow(at indexPath: IndexPath) {
+        texts.remove(at: indexPath.row)
+        self.tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+
+    func editCell(at indexPath: IndexPath) {
+        texts[indexPath.row] = "თიკოს უნდოდა შეცვლა"
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+
+    func addNewCell(at indexPath: IndexPath) {
         
     }
 }
 
 extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        DispatchQueue.main.async {
+            switch indexPath.row {
+            case 0:
+                self.goToColorable(color: .green)
+            case 1:
+                self.goToColorable(color: .red)
+            case 2:
+                self.goToColorable(color: .blue)
+            default:
+                break
+            }
+        }
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        return [
+            UITableViewRowAction(style: .destructive, title: "Delete") { action, path in
+                self.deleteRow(at: path)
+            },
+            UITableViewRowAction(style: .default, title: "Call") { action, path in
+                self.editCell(at: path)
+            },
+            UITableViewRowAction(style: .normal, title: "red") { action, path in
+                print(path)
+            }
+        ]
+    }
     
 }
 
